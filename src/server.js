7b3cf854dab37d1a25813063d1db5041440504f9
omershahzad <omers@xoho.tech>
2021-08-24 11:09:07 +0500

    post request

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import { people } from './people';
-import express from 'express'
-import {promises as fs} from 'fs'
+import express from 'express';
+import bodyParser from 'body-parser';
+import { promises as fs } from 'fs'
 let app = express();
 
+app.use(bodyParser.json()) ;
 //endpoint
 app.get('/hello', (req, res) => {
     res.send("Hello Take some Rest");
@@ -10,10 +12,10 @@ app.get('/hello', (req, res) => {
 
 //get endpoint
 app.get('/people', (req, res) => {
-    res.json(people );
+    res.json(people);
 });
 
-//endpoint name url parameter
+//get endpoint name url parameter
 app.get('/people/:name', (req, res) => {
     let { name } = req.params;
 
@@ -21,10 +23,24 @@ app.get('/people/:name', (req, res) => {
     res.json(person);
 });
 
-//read file data
-app.get('/people', (req, res) => {
+//get read file data
+app.get('/file-data', async (req, res) => {
+    let data = await fs.readFile(__dirname + "/people-data.json");
+    let people = JSON.parse(data);
+    res.json(people);
 
 });
+
+
+//POST endpoint
+app.post('/people', (req, res) => {
+    let newPerson = req.body;
+    people.push(newPerson);
+    res.json(people);
+});
+
+
+
 
 app.listen(3000, () => {
     console.log("server is listining on port 3000");
